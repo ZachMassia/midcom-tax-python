5a8bdf1223a834c6d3130b20e7b004f962eb3305
Zach Massia <zmassia@gmail.com>
2021-04-21 16:19:27 -0400

    Start on products tree UI

--- ui/ui_mainwindow.py
+++ ui/ui_mainwindow.py
@@ -17,7 +17,11 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(1060, 670)
+        self.actionOpen_File = QAction(MainWindow)
+        self.actionOpen_File.setObjectName(u"actionOpen_File")
+        self.actionSave_As = QAction(MainWindow)
+        self.actionSave_As.setObjectName(u"actionSave_As")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.horizontalLayoutWidget = QWidget(self.centralwidget)
@@ -25,20 +29,32 @@ class Ui_MainWindow(object):
         self.horizontalLayoutWidget.setGeometry(QRect(10, 0, 781, 541))
         self.horizontalLayout = QHBoxLayout(self.horizontalLayoutWidget)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setSizeConstraint(QLayout.SetNoConstraint)
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
         self.taxTableView = QTableView(self.horizontalLayoutWidget)
         self.taxTableView.setObjectName(u"taxTableView")
 
         self.horizontalLayout.addWidget(self.taxTableView)
 
+        self.productTreeView = QTreeView(self.horizontalLayoutWidget)
+        self.productTreeView.setObjectName(u"productTreeView")
+
+        self.horizontalLayout.addWidget(self.productTreeView)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 24))
+        self.menubar.setGeometry(QRect(0, 0, 1060, 21))
+        self.menuFile = QMenu(self.menubar)
+        self.menuFile.setObjectName(u"menuFile")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menuFile.addAction(self.actionOpen_File)
+        self.menuFile.addAction(self.actionSave_As)
 
         self.retranslateUi(MainWindow)
 
@@ -47,5 +63,8 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MIDCOM 8000 Tax Editor - Standard Format (20,20,100)", None))
+        self.actionOpen_File.setText(QCoreApplication.translate("MainWindow", u"Open File... (.dat, .str)", None))
+        self.actionSave_As.setText(QCoreApplication.translate("MainWindow", u"Save As...", None))
+        self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
     # retranslateUi
 
